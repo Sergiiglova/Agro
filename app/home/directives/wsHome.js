78d79d8a77b278420514d3be8617fa5ebaa4c067
aLyapricon <sergey.glova@gmail.com>
2017-03-21 15:53:29 +0200

    radius added event for changes added

--- app/home/directives/wsHome.js
+++ app/home/directives/wsHome.js
@@ -10,6 +10,14 @@ angular.module('wise.home').directive('wsHome', function() {
         link: function (scope) {
             scope.data = {};
             scope.data.plantCrop = 25.6
+            scope.data.plantRadius= 12.45;
+            scope.recalc = function() {
+                console.log("recalculated")
+            };
+            scope.$watch('data.plantCrop', scope.recalc);
+
+
+
 
         }
 
