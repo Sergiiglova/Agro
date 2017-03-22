b024ee7111d87217c7343dd559c8c0c2dc793f4d
aLyapricon <sergey.glova@gmail.com>
2017-03-22 19:16:02 +0200

    char moved to home module

--- app/home/controllers/HomeCtrl.js
+++ app/home/controllers/HomeCtrl.js
@@ -1,13 +1,11 @@
 angular.module('wise.home').controller('HomeCtrl', ['$scope', '$filter',
   'ngDialog', '$mdSidenav', '$timeout',
     function ($scope,  $filter,  ngDialog, $mdSidenav, $timeout) {
-        // $scope.hovered = function(d){
-        //     $scope.barValue = d;
-        //     $scope.$apply();
-        // };
-        // $scope.options = {width: 500, height: 300, 'bar': 'aaa'};
-        // $scope.barValue = 'None';
-        // $scope.chartData = [1,2];
-        // $scope.data = [1,2];
+        $scope.options = {width: 500, height: 300, 'bar': 'aaa'};
+        $scope.hovered = function(d){
+            $scope.barValue = d;
+            $scope.$apply();
+        };
+        $scope.barValue = 'None';
 
 }]);
